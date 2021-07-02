a68fec81230065944781e74cb0bd04c751012530
trentgarlipp <trentay@gmail.com>
2021-07-02 13:20:12 -0400

    Added More Inspectables and Title Screen Functionality
    Title screen now works completely with mouse only, See Controls works, Fade transition is seamless, and there are a few more inspectables in the house

--- Assets/_CapstoneAssets/01_Scripts/UI/MainMenuFunctions.cs
+++ Assets/_CapstoneAssets/01_Scripts/UI/MainMenuFunctions.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+    [SerializeField]private Animator fadeAnim;
     [SerializeField]private string sceneName;
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == sceneName)
+        {
+            fadeAnim.SetTrigger("FadeAway");
+        }
+    }
+
     public void StartGame()
+    {
+        fadeAnim.SetTrigger("FadeToWhite");
+        StartCoroutine("WaitForFade");
+    }
+
+    IEnumerator WaitForFade()
+    {
+        yield return new WaitForSeconds(3f);
+        LoadGameScene();
+
+    }
+
+    public void LoadGameScene()
     {
         SceneManager.LoadScene(sceneName);
     }
