705b2592f18612f1c644d03b3c689dfdf6057e13
Michael Fegreus <michael.fegreus@gmail.com>
2021-08-01 16:13:30 -0400

    Driveway Block out, Camera fixes

--- Assets/_CapstoneAssets/01_Scripts/Level/CAMERA_vcam_trigger.cs
+++ Assets/_CapstoneAssets/01_Scripts/Level/CAMERA_vcam_trigger.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class CAMERA_vcam_trigger : MonoBehaviour, IRoomTrigger
 {
 
-    public GameObject myCam;
+    public CinemachineVirtualCamera myCam;
     /*
     [Tooltip("Enable if the script should return to the previous camera on exiting the trigger collider.")]
     public bool returnToLastCam;
-    
+    */
+
+    [Tooltip("Enable if you just want to activate a camera separate from the room system, using this separate from the room system. It will automatically return to the last camera when you exit the volume. ")]
+    [SerializeField] bool affectOnPlayerTrigger = false;
+
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == ("Player"))
         {
-            GAME_manager.Instance.cameraManager.SetNewLevelCamera(myCam);
+            if (affectOnPlayerTrigger)
+            {
+                GAME_manager.Instance.cameraManager.SetNewLevelCamera(myCam);
+            }
         }
     }
 
@@ -20,13 +28,13 @@ public class CAMERA_vcam_trigger : MonoBehaviour, IRoomTrigger
     {
         if (col.tag == ("Player"))
         {
-            if (returnToLastCam)
+            if (affectOnPlayerTrigger)
             {
                 GAME_manager.Instance.cameraManager.SwapToLastLevelCamera();
             }
         }
     }
-    */
+    
     public void OnEnterRoom()
     {
         GAME_manager.Instance.cameraManager.SetNewLevelCamera(myCam);
---
+++ Assets/_CapstoneAssets/01_Scripts/Level/VirtualCameraPlayerSetup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class VirtualCameraPlayerSetup : MonoBehaviour
+{
+    // Set up the virtual camera Follow to the Player Manager instance's sprite object.
+    void Start()
+    {
+        GetComponent<CinemachineVirtualCamera>().Follow = PLAYER_manager.Instance.playerAnimation.spriteModule;
+    }
+}
