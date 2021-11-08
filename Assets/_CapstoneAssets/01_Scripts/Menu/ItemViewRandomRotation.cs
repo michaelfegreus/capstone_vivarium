6e2fcae9b4d18cebc0faf101948e92e58eb64cc8
Michael Fegreus <michael.fegreus@gmail.com>
2021-11-08 09:13:56 -0500

    Opsive Core Setup + Vivarium Integrations

---
+++ Assets/_CapstoneAssets/01_Scripts/Menu/ItemViewRandomRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class ItemViewRandomRotation : MonoBehaviour
+{
+    [SerializeField] Transform rotateTransform;
+
+    [SerializeField] float randomRotationRangeMin;
+    [SerializeField] float randomRotationRangeMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomizeRotation();
+    }
+    private void OnEnable()
+    {
+        RandomizeRotation();
+    }
+
+    public void RandomizeRotation()
+    {
+        rotateTransform.localRotation = Quaternion.Euler(rotateTransform.localRotation.x, rotateTransform.localRotation.y, Random.Range(randomRotationRangeMin, randomRotationRangeMax));
+
+    }
+}
---
+++ Assets/_CapstoneAssets/01_Scripts/OpsiveCustom/EquipperCustom.cs
@@ -0,0 +1,33 @@
+﻿namespace Opsive.UltimateInventorySystem.Equipping
+{
+    using Opsive.Shared.Game;
+    using Opsive.UltimateInventorySystem.Core;
+    using Opsive.UltimateInventorySystem.Core.DataStructures;
+    using Opsive.UltimateInventorySystem.Core.InventoryCollections;
+    using Opsive.UltimateInventorySystem.Storage;
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using EventHandler = Opsive.Shared.Events.EventHandler;
+
+    public class EquipperCustom : Equipper
+    {
+        public Item item;
+
+        public void FieldEquip()
+        {
+            //Check for available empty slots.
+            for (int i = 0; i < m_Slots.Length; i++)
+            {
+                if (m_Slots[i].ItemObject != null) { continue; }
+                if (m_Slots[i].Category != null && m_Slots[i].Category.InherentlyContains(item) == false) { continue; }
+            }
+
+            //Check for any slot (even used ones).
+            for (int i = 0; i < m_Slots.Length; i++)
+            {
+                if (m_Slots[i].Category != null && m_Slots[i].Category.InherentlyContains(item) == false) { continue; }
+            }
+        }
+    }
+}
