41178885a595e13102109cbd7ee81166ad7dc24b
Michael Fegreus <michael.fegreus@gmail.com>
2022-08-16 00:59:13 -0400

    Game Management workflow changes, Bridge Progress

---
+++ Assets/_CapstoneAssets/01_Scripts/Debug/EnableCanvasOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EnableCanvasOnPlay : MonoBehaviour
+{
+    // Use this for Editor purposes so you can keep UI canvases disabled off in the Game viewport until you start the game.
+
+    Canvas myCanvas;
+
+    void Start()
+    {
+        myCanvas = GetComponent<Canvas>();
+    }
+}
