78096349fd07dd0f597c50f32379c8f1d8a3f3e4
trentgarlipp <trentay@gmail.com>
2023-02-08 09:01:26 -0500

    Implemented New Backgrounds, Fixed Bridge Fading Problems
    Implemented Shed, Driveway, Garden, Cottage and Garden backgrounds, fixed some bugs, reimplemented parallax, and fixed the bridge fade issues.

---
+++ Assets/_CapstoneAssets/01_Scripts/Camera/CopyCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyCamera : MonoBehaviour
+{
+    private Camera cam;
+    private Camera mainCam;
+    [SerializeField] private float fieldOfViewMultipllier = 1;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+        mainCam = Camera.main;
+    }
+    void Update()
+    {
+        cam.transform.position = mainCam.transform.position;
+        cam.transform.rotation = mainCam.transform.rotation;
+        cam.orthographicSize = mainCam.orthographicSize * fieldOfViewMultipllier;
+    }
+}
