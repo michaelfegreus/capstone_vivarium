c9051c30a1542f038ad6aa7b3cb37e65c88d7a79
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-12-26 17:06:33 -0500

    render texture prototype test

---
+++ Assets/Textures/RenderTextureUpdate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextureUpdate : MonoBehaviour
+{
+    [SerializeField] CustomRenderTexture ourTex;
+    [SerializeField] int frameRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RunCustomUpdate());
+    }
+
+    // our custom update
+    IEnumerator RunCustomUpdate()
+    {
+        // run at our framerate
+        yield return new WaitForSecondsRealtime(frameRate / 1);
+        // run the update
+        CustomUpdate();
+        // loop
+        StartCoroutine(RunCustomUpdate());
+    }
+
+    void CustomUpdate()
+    {
+        ourTex.Update();
+    }
+}
