986c91c5a56ea0773c52aaf308de33cc493b7242
trentgarlipp <trentay@gmail.com>
2024-01-12 10:00:32 -0500

    Marion animations, animation events and script, Catfish fishing animations and convos, sprite reflection scripts for NPCs, ground cracks for shovelling, and more for the low price of $9.99!!!!

---
+++ Assets/_CapstoneAssets/01_Scripts/NPC/SpriteReflection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteReflection : MonoBehaviour
+{
+
+    public Animator anim;
+
+    [SerializeField] private Transform reflectionTransform;
+
+    private SpriteRenderer regularSprite;
+    private SpriteRenderer reflectSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        regularSprite = anim.GetComponent<SpriteRenderer>();
+        reflectSprite = reflectionTransform.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reflectSprite.sprite = regularSprite.sprite;
+    }
+}
