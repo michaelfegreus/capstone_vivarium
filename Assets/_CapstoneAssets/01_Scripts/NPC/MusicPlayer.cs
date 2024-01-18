4ed915254a230cf17b5b91184c7f8fd66ec0d01a
Josh <35745910+Joshmh7128@users.noreply.github.com>
2024-01-18 18:23:27 -0500

    working on music system

---
+++ Assets/_CapstoneAssets/01_Scripts/NPC/MusicPlayer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Josh
+{
+    [RequireComponent(typeof(AudioSource))]
+    /// <summary>
+    /// This is the MusicPlayer class. It plays the music in the scene to a 2D Audio source. 
+    /// This script plays songs based on time of day, the current room, and manually triggered events.
+    /// It fades between songs. You can also manually trigger the start and stop of music, or silence.
+    /// </summary>
+    public class MusicPlayer : MonoBehaviour
+    {
+        /// <summary>
+        /// The public static instance of the MusicPlayer class
+        /// </summary>
+        public static MusicPlayer instance;
+        void Awake() => instance = this;
+
+        /// <summary>
+        /// Our current music playback dataset. This must be set in the editor!
+        /// </summary>
+        public MusicPlaybackDataset currentPlaybackDataset;
+
+        /// <summary>
+        /// What possible times are there in the environment?
+        /// Used by the TimePool class to mark what time the pool represents
+        /// </summary>
+        public enum PossibleTimes
+        {
+            Morning, Midday, Afternoon, Night
+        }
+
+        /// <summary>
+        /// What possible Room Clusters are there in the game?
+        /// Used by the RoomCluster class to mark which room cluster is associated with it in the game
+        /// </summary>
+        public enum PossibleRoomClusters
+        {
+            House, Town, TreeHollow, Forest,
+
+            // leave this here so that we can get our maximum count of room clusters
+            Count
+        }
+
+        /// <summary>
+        /// Function plays music based off of: Time of day, current room, and previously played song.
+        /// Calling this function without any arguments causes it to automatically determine these features.
+        /// </summary>
+        public void PlayMusic()
+        {
+
+        }
+
+        /// <summary>
+        /// Function plays music based off of: Time of day, current room, and previously played song.
+        /// Calling this function without any arguments causes it to automatically determine these features.
+        /// Calling this function with an AudioClip overrides the current song and plays that clip instead.
+        /// </summary>
+        /// <param name="song"></param>
+        public void PlayMusic(AudioClip song)
+        {
+
+        }        
+        
+        /// <summary>
+        /// Function plays music based off of: Time of day, current room, and previously played song.
+        /// Calling this function without any arguments causes it to automatically determine these features.
+        /// Calling this function with an AudioClip overrides the current song and plays that clip instead.
+        /// Calling this function with maxLoops sets the maximum randomized loops that this clip can play in a row.
+        /// </summary>
+        /// <param name="song"></param>
+        public void PlayMusic(AudioClip song, int maxLoops)
+        {
+
+        }
+
+        /// <summary>
+        /// Function plays music based off of: Time of day, current room, and previously played song.
+        /// Calling this function without any arguments causes it to automatically determine these features.
+        /// Calling this function with a MusicClip plays the audio using the properties of that MusicClip
+        /// </summary>
+        /// <param name="musicClip"></param>
+        public void PlayMusic(MusicClip musicClip)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// RoomClusters hold the selection of Time Pools for each cluster of rooms in the game.
+    /// </summary>
+    [System.Serializable]
+    public class RoomCluster
+    {
+        public List<TimePool> timePools = new List<TimePool>()
+        {
+            new TimePool(MusicPlayer.PossibleTimes.Morning, new List<MusicClip>()),
+            new TimePool(MusicPlayer.PossibleTimes.Midday, new List<MusicClip>()),
+            new TimePool(MusicPlayer.PossibleTimes.Afternoon, new List<MusicClip>()),
+            new TimePool(MusicPlayer.PossibleTimes.Night, new List<MusicClip>())
+        };
+    }
+    /// <summary>
+    /// TimePools hold collections of songs for each time of day, to be held by RoomClusters
+    /// </summary>
+    [System.Serializable]
+    public class TimePool
+    {
+        /// <summary>
+        /// Referenced by other scripts to determine which time this pool represents.
+        /// Should only have one TimePool per time 
+        /// </summary>
+        public MusicPlayer.PossibleTimes setTime;
+        public List<MusicClip> musicClips = new List<MusicClip>();
+
+        public TimePool(MusicPlayer.PossibleTimes setTime, List<MusicClip> musicClips)
+        {
+            this.setTime = setTime;
+            this.musicClips = musicClips;
+        }
+    }
+
+    /// <summary>
+    /// This is a custom class which holds the information for each Music track to be played back correctly.
+    /// This information is a public class which holds a series of public variables to playback music
+    /// </summary>
+    [System.Serializable]
+    [SerializeField] public class MusicClip 
+    {
+        public AudioClip musicClip; // what is the audio we want to play?
+        public int maxLoops; // how many times can this loop at most?
+        public bool loopsForever; // does this loop forever? sets the loop count to 9999
+    }
+
+    /// <summary>
+    /// This is our scriptable object which holds every single: Room Cluster, Time Pool, and MusicClip
+    /// </summary>
+    [CreateAssetMenu(fileName = "MusicPlaybackData", menuName = "Music System/MusicPlaybackData", order = 1)]
+    public class MusicPlaybackDataset : ScriptableObject
+    {
+        // build our list of room clusters, this will auto-populate
+        public List<RoomCluster> roomClusters = new List<RoomCluster>();
+    }
+}
