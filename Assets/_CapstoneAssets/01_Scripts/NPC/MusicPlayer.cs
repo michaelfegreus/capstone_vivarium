82596ba3fb38307e625b862a984c4cab81d59cad
Josh <35745910+Joshmh7128@users.noreply.github.com>
2024-01-19 11:02:49 -0500

    ensuring the music dataset is its own class in an individual unity script so that the data can be more easily transferred

--- Assets/_CapstoneAssets/01_Scripts/NPC/MusicPlayer.cs
+++ Assets/_CapstoneAssets/01_Scripts/NPC/MusicPlayer.cs
@@ -149,39 +149,5 @@ namespace Josh
         }
     }
 
-    /// <summary>
-    /// This is our scriptable object which holds every single: Room Cluster, Time Pool, and MusicClip
-    /// </summary>
-    [CreateAssetMenu(fileName = "MusicPlaybackData", menuName = "Music System/MusicPlaybackData", order = 1)]
-    public class MusicPlaybackDataset : ScriptableObject
-    {
-        // build our list of room clusters, this will auto-populate
-        public List<RoomCluster> roomClusters = new List<RoomCluster>();
 
-        void Awake()
-        {
-            ValidateDataset();
-        }
-
-        // when we validate this object
-        void OnValidate()
-        {
-            ValidateDataset();
-        }
-
-        void ValidateDataset()
-        {
-            if (roomClusters.Count < (int)MusicPlayer.PossibleRoomClusters.Count)
-            {
-                Debug.Log("it's lower than 4.");
-                roomClusters.Clear();
-                // then
-                for (int i = 0; i < (int)MusicPlayer.PossibleRoomClusters.Count; i++)
-                {
-                    // create one new RoomCluster with the correct header per room cluster area
-                    roomClusters.Add(new RoomCluster(Enum.GetName(typeof(MusicPlayer.PossibleRoomClusters), i)));
-                }
-            }
-        }
-    }
 }
---
+++ Assets/_CapstoneAssets/09_ScriptableObjects/MusicPlaybackDataset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Josh;
+
+namespace Josh
+{
+    /// <summary>
+    /// This is our scriptable object which holds every single: Room Cluster, Time Pool, and MusicClip
+    /// </summary>
+    [CreateAssetMenu(fileName = "MusicPlaybackDataset", menuName = "Music System/MusicPlaybackData", order = 1)]
+    public class MusicPlaybackDataset : ScriptableObject
+    {
+        // build our list of room clusters, this will auto-populate
+        public List<RoomCluster> roomClusters = new List<RoomCluster>();
+
+        void Awake()
+        {
+            ValidateDataset();
+        }
+
+        // when we validate this object
+        void OnValidate()
+        {
+            ValidateDataset();
+        }
+
+        void ValidateDataset()
+        {
+            if (roomClusters.Count < (int)MusicPlayer.PossibleRoomClusters.Count)
+            {
+                UnityEngine.Debug.Log("it's lower than 4.");
+                roomClusters.Clear();
+                // then
+                for (int i = 0; i < (int)MusicPlayer.PossibleRoomClusters.Count; i++)
+                {
+                    // create one new RoomCluster with the correct header per room cluster area
+                    roomClusters.Add(new RoomCluster(Enum.GetName(typeof(MusicPlayer.PossibleRoomClusters), i)));
+                }
+            }
+        }
+    }
+}
