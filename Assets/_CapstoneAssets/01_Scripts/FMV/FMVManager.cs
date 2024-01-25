212ac3d75aa429d1b4da21636810a565948769f9
trentgarlipp <trentay@gmail.com>
2024-01-25 17:15:06 -0500

    Exported a bunch of animations for Jenny, added in driveway foreground terrarium glass and wood stuff, added in the FMV cutscene for the tree grove

---
+++ Assets/_CapstoneAssets/01_Scripts/FMV/FMVManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class FMVManager : MonoBehaviour
+{
+
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    public void PlayVideo()
+    {
+        videoPlayer.Play();
+        PlayerManager.Instance.EnterMenuState();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        videoPlayer.Stop();
+        PlayerManager.Instance.EnterFreeState();
+    }
+
+    void Update()
+    {
+        
+    }
+}
