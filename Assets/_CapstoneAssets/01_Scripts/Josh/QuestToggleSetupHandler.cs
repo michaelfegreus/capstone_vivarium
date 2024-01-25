83c9bbbd8347864586dc7b73b484a12ea593e399
Josh <35745910+Joshmh7128@users.noreply.github.com>
2024-01-25 15:34:06 -0500

    testing toggle work

---
+++ Assets/_CapstoneAssets/01_Scripts/Josh/QuestToggleSetupHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class QuestToggleSetupHandler : MonoBehaviour
+{
+    [SerializeField] Toggle toggle; // our toggle
+    QuestTrackerCompanion trackerCompanion; // our quest tracker companion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle ??= GetComponent<Toggle>();
+
+        // find and setup this toggle
+        toggle.onValueChanged.AddListener(delegate { trackerCompanion.ToggleQuests(); });
+    }
+
+
+    void ToggleQuests()
+    {
+
+    }
+}
---
+++ Assets/_CapstoneAssets/01_Scripts/Josh/QuestTrackerCompanion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrackerCompanion : MonoBehaviour
+{
+    // variables
+    [SerializeField] CanvasGroup questGroup; // the UI canvas group for our quests
+    float targetAlpha, fadeRate = 1;
+
+    // public functions for showing / hiding the active quests
+    public void ShowQuests()
+    {
+        targetAlpha = 1f;
+    }
+
+    // hide the quests
+    public void HideQuests()
+    {
+        targetAlpha = 0;
+    }
+
+    public void ToggleQuests()
+    {
+        if (targetAlpha == 1)
+            HideQuests();
+        if (targetAlpha == 0)
+            ShowQuests();
+    }
+
+    // run the fixed update 60 times per second
+    void FixedUpdate()
+    {
+        // fade to our desired canvas alpha state
+        questGroup.alpha = questGroup.alpha == targetAlpha ? Mathf.MoveTowards(questGroup.alpha, targetAlpha, fadeRate * Time.deltaTime) : questGroup.alpha;
+    }
+}
