d0f9cc4d4af6151758c87862825a4fe656127f9f
Josh <35745910+Joshmh7128@users.noreply.github.com>
2024-01-26 11:32:43 -0500

    setup debug teleport

---
+++ Assets/_CapstoneAssets/01_Scripts/Josh/DebugMover.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DebugMover : MonoBehaviour
+{
+    [SerializeField] List<Vector2> teleportLocations;
+
+    void Update()
+    {
+        TeleportCheck();
+    }
+
+    void TeleportCheck()
+    {
+        KeyCode locationCode;
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            // check through all our keys and see which one is being pressed
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (kcode == KeyCode.T) continue;
+
+                if (Input.GetKey(kcode))
+                {
+                    locationCode = kcode;
+                    // teleport to the correlating location
+                    Teleport(teleportLocations[(int)kcode-49]);
+                    break;
+                }
+            }
+        }
+
+    }
+
+    void Teleport(Vector2 target)
+    {
+        transform.position = target;
+    }
+}
