f261dbb3cfa30cbe8072edb27eab543f66ca6699
michaelfegreus <michael.fegreus@gmail.com>
2024-02-14 15:51:42 -0500

    cached work

---
+++ Assets/_CapstoneAssets/01_Scripts/Level/WarpParallaxCatch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class WarpParallaxCatch : MonoBehaviour
+{
+    [Tooltip("Make this object the focus of the parallax")]
+    [SerializeField]
+    CinemachineVirtualCamera myCamera;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Trim().Equals("Player".Trim()))
+        {
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag.Trim().Equals("Player".Trim()))
+        {
+
+        }
+
+    }
+}
