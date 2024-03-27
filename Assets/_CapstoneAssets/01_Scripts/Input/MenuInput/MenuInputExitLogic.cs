da902f71855105554f2818960c3c39b6b84fc117
trentgarlipp <trentay@gmail.com>
2024-03-27 15:01:34 -0400

    Added smoke to Chef's cigarette. Made menus easier to exit by pressing the cancel button once if you're already on the exit button, or twice if you're not. Added a bunch of the GameBoy/DigiCart quest stuff. Fixed workshop bug where you can get behind the door to the outside. Fixed bug where you can run behind the shed. Fixed bug where hotbar is invisibly open at start of scene. Maybe fixed bug where the mirror scene can softlock you

---
+++ Assets/_CapstoneAssets/01_Scripts/Input/MenuInput/MenuInputExitLogic.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+//Genius God of a Man Trent ©
+
+public class MenuInputExitLogic : MonoBehaviour
+{
+    MenuInput exitInput;
+    [SerializeField] private Button[] exitButtons;
+
+    void Start()
+    {
+        exitInput = new MenuInput();
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        if (exitInput != null)
+        {
+            exitInput.Enable();
+            exitInput.InventoryEngine.Cancel.performed += ExitMenu;
+        }
+    }
+
+    private void OnDisable()
+    {
+        exitInput.Disable();
+        exitInput.InventoryEngine.Cancel.performed -= ExitMenu;
+    }
+
+    void ExitMenu(InputAction.CallbackContext obj)
+    {
+        GameObject currentButton;
+        currentButton = EventSystem.current.currentSelectedGameObject;
+
+        if (currentButton.name.Equals("Exit Button"))
+        {
+            currentButton.GetComponent<Button>().onClick.Invoke();
+        }
+        else
+        {
+            foreach (Button b in exitButtons)
+            {
+                if (b.isActiveAndEnabled)
+                {
+                    EventSystem.current.SetSelectedGameObject(b.gameObject);
+                }
+            }
+        }
+
+    }
+}
